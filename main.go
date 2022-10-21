afb4a83044cf57bd710d5655f5bbad1f01524c43
Takuya Mannami <takuya.mannami@gmail.com>
2022-10-21 23:00:37 +0900

    rename

---
+++ main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+)
+
+func Check() {
+	if len(os.Args) != 2 {
+		fmt.Fprintln(os.Stderr, "argument error")
+		os.Exit(1)
+	}
+
+	url := os.Args[1]
+	resp, err := http.Get(url)
+
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+
+	defer resp.Body.Close()
+	xCache := resp.Header.Get("x-cache")
+
+	if xCache == "" {
+		fmt.Fprintln(os.Stderr, "x-cache header not found")
+	} else {
+		fmt.Println(xCache)
+	}
+}
